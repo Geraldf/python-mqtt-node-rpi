63d7566211480607a87a3924b2c6300793ea3bd3
Gerald Fuchs <gerald@fuchsclan.de>
2021-12-31 10:32:15 +0100

    newstart

--- mqtt_node/__main__.py
+++
@@ -1,3 +0,0 @@
-from client.main import run_client
-
-run_client()
--- mqtt_node/io/control.py
+++
@@ -1,7 +0,0 @@
-from . import gpio
-
-def set_io(pin, state):
-    return int(gpio.write(pin, state)) == 0
-
-def get_io(pin):
-    return gpio.read(pin)
